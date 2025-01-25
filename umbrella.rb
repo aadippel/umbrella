b2df2a56650df0b2331f364f90efbc8410d95a8b
aadippel <aadippel@gmail.com>
2025-01-25 22:28:40 +0000

    added gmaps url

--- umbrella.rb
+++ umbrella.rb
@@ -2,3 +2,14 @@ require "http"
 require "json"
 require "dotenv/load"
 # Write your soltuion here!
+pp "Hello!"
+pp "Where are you located?"
+
+user_location = "Chicago"
+
+# user_location = gets.chomp
+
+pp user_location
+
+maps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + user_location + "&key=" + ENV.fetch("GMAPS_KEY")
+p maps_url

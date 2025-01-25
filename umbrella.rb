57e1a2587643f4e260aa82201957a76f977b7a35
aadippel <aadippel@gmail.com>
2025-01-25 22:35:59 +0000

    added raw and parsed responses

--- umbrella.rb
+++ umbrella.rb
@@ -13,3 +13,15 @@ pp user_location
 
 maps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + user_location + "&key=" + ENV.fetch("GMAPS_KEY")
 p maps_url
+
+require "http"
+
+resp = HTTP.get(maps_url)
+
+raw_response = resp.to_s
+
+require "json"
+
+parsed_response = JSON.parse(raw_response)
+
+pp parsed_response.keys
